5bcd4488f356b37286d39cd4995d3af4b82d39f9
Mushrif-iOS <i.binarybrill@gmail.com>
2025-04-01 01:56:24 +0530

    Final code

---
+++ Mushrif Cafe/View/Profile/CellClass/ManageListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ManageListTableViewCell.swift
+//  Mushrif Cafe
+//
+//  Created by Bhushan Kumar on 01/04/25.
+//
+
+import UIKit
+
+class ManageListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
